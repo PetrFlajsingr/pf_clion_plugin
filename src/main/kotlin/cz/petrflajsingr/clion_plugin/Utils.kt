43c9d7ca083b07dd880f2357716842a2feaef8d0
PetrFlajsingr <flajsingr@gmail.com>
2023-02-12 01:33:35 +0100

    Change assert id generation

--- src/main/kotlin/cz/petrflajsingr/clion_plugin/Utils.kt
+++ src/main/kotlin/cz/petrflajsingr/clion_plugin/Utils.kt
@@ -3,5 +3,5 @@ package cz.petrflajsingr.clion_plugin
 import kotlin.random.Random
 
 fun generateRandomAssertID(): String {
-    return "0x%017X_aID".format(Random.nextLong())
+    return "0x%017X".format(Random.nextLong())
 }
